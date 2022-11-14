a6fd34ad89e7b00cb15872cdd2323f482aa9ae77
Enis Tara <eniss.tara@gmail.com>
2022-11-14 23:46:40 +0100

    Created few more basic integration tests

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 const app = require("./app");
 const port = process.env.BACKEND_API_SERVER_PORT || 8080;
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+const main = (port) => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit();
+  }
+}
+
+main(port);
--- tests/app.test.js
+++ tests/app.test.js
@@ -10,4 +10,106 @@ describe("Employees API", () => {
       .expect("Content-Type", /json/)
       .expect(HttpStatus.Ok, done);
   });
+
+  it("GET /api/v1/employees/1 --> should respond with json", (done) => {
+    supertest(app)
+      .get("/api/v1/employees/1")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(HttpStatus.Ok, done)
+  });
+
+  it("GET /api/v1/employees/404999 --> should respond with [] if not found", (done) => {
+    supertest(app)
+      .get("/api/v1/employees/404999")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(HttpStatus.Ok)
+      .expect((response) => {
+        expect(response.body.data.length).toEqual(0)
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  it("GET /api/v1/employees/search/term --> should respond with json", (done) => {
+    supertest(app)
+      .get("/api/v1/employees/search/term")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(HttpStatus.Ok, done);
+  });
+
+  it("GET /api/v1/search/0 --> should respond with [] if not found", (done) => {
+    supertest(app)
+      .get("/api/v1/employees/search/0")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(HttpStatus.Ok)
+      .expect((response) => {
+        expect(response.body.data.length).toEqual(0)
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+});
+
+describe("Roles API", () => {
+  it("GET /api/v1/roles --> should respond with json", (done) => {
+    supertest(app)
+      .get("/api/v1/roles")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(HttpStatus.Ok, done);
+  });
+
+  it("GET /api/v1/roles/1 --> should respond with json", (done) => {
+    supertest(app)
+      .get("/api/v1/roles/1")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(HttpStatus.Ok, done)
+  });
+
+  it("GET /api/v1/roles/404999 --> should respond with [] if not found", (done) => {
+    supertest(app)
+      .get("/api/v1/roles/404999")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(HttpStatus.Ok)
+      .expect((response) => {
+        expect(response.body.data.length).toEqual(0)
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  it("GET /api/v1/roles/search/term --> should respond with json", (done) => {
+    supertest(app)
+      .get("/api/v1/roles/search/term")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(HttpStatus.Ok, done);
+  });
+
+  it("GET /api/v1/roles/0 --> should respond with [] if not found", (done) => {
+    supertest(app)
+      .get("/api/v1/roles/search/0")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(HttpStatus.Ok)
+      .expect((response) => {
+        expect(response.body.data.length).toEqual(0)
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
 });
